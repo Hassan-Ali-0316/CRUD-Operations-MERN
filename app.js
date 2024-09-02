557c035f8add377afbc0bb5a6446e7e130eb452a
Hassan-Ali-0316 <ha159193@gmail.com>
2024-09-02 15:59:53 +0500

    updated files

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path'); 
 const app = express();
+const userModel = require('./models/user');
+const user = require('./models/user');
 
 app.set('view engine','ejs');
 app.use(express.json());
@@ -11,8 +13,23 @@ app.use(express.static(path.join(__dirname,'public')));
 app.get('/',function(req,res){
     res.render("index");
 })
-app.get('/read',function(req,res){
-    res.render("read");
+app.get('/read',async function(req,res){
+    let rusers = await userModel.find();
+    res.render("read",{users:rusers});
+})
+
+app.post('/create',async function(req,res){
+    let {name,email,image} = req.body;
+    let cuser = await userModel.create({
+        name:name,
+        email:email,
+        image:image
+    })
+    res.render('/read');
+})
+app.get('/delete/:id',async function(req,res){
+    let duser = await userModel.findOneAndDelete({_id: req.params.id});
+    res.redirect("/read");
 })
 
 app.listen('3000');
